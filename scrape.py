d2b7965b7e8110f9558f2cee11b27a8567cf0b94
Stefano Rivera <stefano@rivera.za.net>
2020-07-10 16:06:52 -0700

    Album scraping

--- scrape.py
+++ scrape.py
@@ -26,6 +26,65 @@ def main():
         sys.exit('Interrupted')
 
 
+def extract_albums(album_url, soup):
+    untitled_index = 0
+    for album in soup.find_all('td', class_='giAlbumCell'):
+        url = urljoin(album_url, album.a['href'])
+
+        desc_block = album.next_sibling
+        if isinstance(desc_block, NavigableString):
+            desc_block = desc_block.next_sibling
+        if desc_block is None:
+            desc_block = album
+
+        title = desc_block.find('p', class_='giTitle')
+        if title:
+            title = title.text.strip()
+            title = re.sub(r'^Album: ', '', title)
+        else:
+            title = 'Untitled Album {}'.format(untitled_index)
+            untitled_index += 1
+
+        info_block = desc_block.find('div', class_='giInfo')
+        owner = None
+        if info_block:
+            owner_block = info_block.find('div', class_='owner')
+            if owner_block:
+                owner = owner_block.text.strip()
+                owner = re.sub(r'^Owner: ', '', owner)
+
+        yield {
+            'url': url,
+            'title': title,
+            'owner': owner,
+        }
+
+
+def extract_images(album_url, soup):
+    for img in soup.find_all('td', class_='giItemCell'):
+        url = urljoin(album_url, img.a['href'])
+
+        desc_block = img.next_sibling
+        if isinstance(desc_block, NavigableString):
+            desc_block = desc_block.next_sibling
+        if desc_block is None:
+            desc_block = img  # WTF
+
+        title = desc_block.find('p', class_='giTitle')
+        if title:
+            title = title.text.strip()
+
+        description = desc_block.find('p', class_='giDescription')
+        if description:
+            description = description.text.strip()
+
+        yield {
+            'url': url,
+            'title': title,
+            'description': description,
+        }
+
+
 class Scraper:
     def __init__(self, url):
         self.base_url = url
@@ -37,52 +96,39 @@ class Scraper:
         main_url = urljoin(self.base_url, 'main.php')
         if not output.exists():
             output.mkdir()
-        self.scrape_album(main_url, output, title='', owner='DebConf')
+        self.scrape_album(output, {
+            'title': '',
+            'owner': 'DebConf',
+            'url': main_url,
+        })
 
     def slug(self, string):
         slug = re.sub(r'[^A-Za-z0-9 ]*', '', string)
         slug = re.sub(r'\s+', '-', slug)
         return slug[:100]
 
-    def scrape_album(self, album_url, parent_path, title, owner):
-        slug = self.slug(title)
+    def scrape_album(self, parent_path, album):
+        slug = self.slug(album['title'])
         path = parent_path / slug
         if not path.exists():
             path.mkdir()
 
-        meta = {
-            'title': title,
-            'owner': owner,
-        }
         meta_fn = path / 'gallery_meta.yml'
         if meta_fn.exists():
             return
-        print('\nDownloading {} -> {}\n{}'.format(title, path, album_url))
+        print('\nDownloading {} -> {}\n{}'.format(
+                album['title'], path, album['url']))
 
-        self.scrape_sub_albums(album_url, path)
-        index = 0
-        for soup in self.paginated_soup(album_url):
-            for img in soup.find_all('td', class_='giItemCell'):
-                url = urljoin(album_url, img.a['href'])
-                desc_block = img.next_sibling
-                if isinstance(desc_block, NavigableString):
-                    desc_block = desc_block.next_sibling
-                if desc_block is None:
-                    desc_block = img  # WTF
-                title = desc_block.find('p', class_='giTitle')
-                if title:
-                    title = title.text.strip()
-                description = desc_block.find('p', class_='giDescription')
-                if description:
-                    description = description.text.strip()
-                self.scrape_image(url, title, description, path, index)
-                index += 1
+        self.scrape_sub_albums(album['url'], path)
+        for soup in self.paginated_soup(album['url']):
+            for i, image in enumerate(extract_images(album['url'], soup)):
+                self.scrape_image(i, image, path)
 
         with meta_fn.open('w') as f:
-            yaml.safe_dump(meta, f, default_flow_style=False)
+            yaml.safe_dump(album, f, default_flow_style=False)
 
-    def scrape_image(self, url, title, description, path, index):
-        m = re.match(r'.*[?&]g2_itemId=(\d+)(?:&.*|$)', url)
+    def scrape_image(self, index, image, path):
+        m = re.match(r'.*[?&]g2_itemId=(\d+)(?:&.*|$)', image['url'])
         id_ = m.group(1)
         dl_url = urljoin(self.base_url, 'main.php?' + urlencode({
             'g2_view': 'core.DownloadItem',
@@ -90,8 +136,8 @@ class Scraper:
         }))
 
         basename = '{:03}'.format(index)
-        if title:
-            basename += '-' + self.slug(title)
+        if image['title']:
+            basename += '-' + self.slug(image['title'])
         meta_fn = path / (basename + '.yml')
 
         if meta_fn.exists():
@@ -121,40 +167,14 @@ class Scraper:
             for chunk in r.iter_content(chunk_size=1024):
                 f.write(chunk)
 
-        meta = {
-            'title': title,
-            'description': description,
-        }
         with meta_fn.open('w') as f:
-            yaml.safe_dump(meta, f, default_flow_style=False)
+            yaml.safe_dump(image, f, default_flow_style=False)
 
 
     def scrape_sub_albums(self, url, path):
-        untitled_index = 0
         for soup in self.paginated_soup(url):
-            for album in soup.find_all('td', class_='giAlbumCell'):
-                url = urljoin(url, album.a['href'])
-                desc_block = album.next_sibling
-                if isinstance(desc_block, NavigableString):
-                    desc_block = desc_block.next_sibling
-                if desc_block is None:
-                    desc_block = album
-
-                title = desc_block.find('p', class_='giTitle')
-                if title:
-                    title = title.text.strip()
-                    title = re.sub(r'^Album: ', '', title)
-                else:
-                    title = 'Untitled Album {:2}'.format(untitled_index)
-                    untitled_index += 1
-
-                info_block = desc_block.find('div', class_='giInfo')
-                if info_block:
-                    owner = info_block.find('div', class_='owner').text.strip()
-                    owner = re.sub(r'^Owner: ', '', owner)
-                else:
-                    owner = None
-                self.scrape_album(url, path, title, owner)
+            for album in extract_albums(url, soup):
+                self.scrape_album(path, album)
 
     def paginated_soup(self, url):
         while True:
